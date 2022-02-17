14428a0677d063884bb152f984cbfa7b3158c7f2
Rafael Tieppo <82878718+rafaeltieppo@users.noreply.github.com>
2022-02-17 07:53:59 -0300

    Portfolio

--- home/index.js
+++ home/index.js
@@ -1,12 +1,15 @@
 var modal = document.querySelector(".modal");
 var gmail = document.querySelector(".gmail-link");
+var body = document.querySelector("body");
 
 function showMenu() {
     modal.style.display = "flex";
     gmail.style.display = "none";
+    body.style.overflow = "hidden";
 }
 
 function closeMenu() {
     modal.style.display = "none";
     gmail.style.display = "block";
+    body.style.overflow = "scroll";
 }
